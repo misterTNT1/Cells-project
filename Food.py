cb6d02b84f0fc212b312306e18947a74473496d3
Eyal Marom <140832353+misterTNT1@users.noreply.github.com>
2024-02-14 15:28:45 +0200

    Update Food.py

--- Food.py
+++ Food.py
@@ -1,3 +1,18 @@
+from random import random, randint
+
+def gen_location(l1, size=SIZE): #this is the same function that is in Cell.py
+  if l1 is food_locations:
+    location = [randint(-size / 2, size /2 ), randint(-size / 2, size / 2)]
+    while location in food_locations:
+      location = [randint(-size / 2, size /2 ), randint(-size / 2, size / 2)]
+    food_locations.append(location)
+  else:
+    location = [randint(-size / 2, size /2 ), randint(-size / 2, size / 2)]
+    while location in cell_locations:
+      location = [randint(-size / 2, size /2 ), randint(-size / 2, size / 2)]
+    cell_locations.append(location)
+  return location
+
 class Food:
   global AMOUNT
   def __init__(self, food_per_t):
